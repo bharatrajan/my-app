1a29a5100cfec7a2b725d346a5f103fd89849556
Rajan, Bharat (Contractor) <Bharat_Rajan@comcast.com>
2018-03-05 16:00:12 -0600

    Eslint & Prettification

--- src/api/api.js
+++ src/api/api.js
@@ -1,38 +1,38 @@
-import backboneRankList from '../mocks/backboneRankList';
-import customerRankList from '../mocks/customerRankList';
-import growthRankList from '../mocks/growthRankList';
-import peeringRankList from '../mocks/peeringRankList';
-import retailRankList from '../mocks/retailRankList';
-import wholesaleRankList from '../mocks/wholesaleRankList';
+import backboneRankList from "../mocks/backboneRankList";
+import customerRankList from "../mocks/customerRankList";
+import growthRankList from "../mocks/growthRankList";
+import peeringRankList from "../mocks/peeringRankList";
+import retailRankList from "../mocks/retailRankList";
+import wholesaleRankList from "../mocks/wholesaleRankList";
 
 //API path
-const apiServer = "/api"
+const apiServer = "/api";
 
 //Request headers
 const headers = {
-  'Accept': 'application/json',
-  'cache-control': 'no-cache',
-}
+  Accept: "application/json",
+  "cache-control": "no-cache"
+};
 
 //Dev enviroment deduction
-export const isDevEnv = (window.location.host.indexOf('localhost:3000') === 0)
+export const isDevEnv = window.location.host.indexOf("localhost:3000") === 0;
 
 //Response factory
 let mockResponse = {
-    backbone : backboneRankList,
-    customer : customerRankList,
-    growth : growthRankList,
-    peering : peeringRankList,
-    retail : retailRankList,
-    wholesale : wholesaleRankList
-  }
+  backbone: backboneRankList,
+  customer: customerRankList,
+  growth: growthRankList,
+  peering: peeringRankList,
+  retail: retailRankList,
+  wholesale: wholesaleRankList
+};
 
 //NON-AJAX way only for development
 export const getRankList = type => {
-  return new Promise((res) => {
-    res(mockResponse[type])
-  })
-}    
+  return new Promise(res => {
+    res(mockResponse[type]);
+  });
+};
 
 //fetch based API Calling
 //Should be used in Production
@@ -41,26 +41,25 @@ export const fetchRankList = type => {
   fetch(`${apiServer}/${type}`, { headers })
     .then(res => res.json())
     .then(data => data.books)
-    .catch(error => error)   
-}
+    .catch(error => error);
+};
 
 //Simple XHR API calls
 export const getRankListXHR = (type, successCB, failureCB) => {
   let url = `mocks/${type}.json`;
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
-      if (xhr.readyState == XMLHttpRequest.DONE) {
-          try{
-            let resp = JSON.parse(xhr.responseText)
-            successCB(resp)
-          }catch(err){
-            failureCB(err)
-          } 
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      try {
+        let resp = JSON.parse(xhr.responseText);
+        successCB(resp);
+      } catch (err) {
+        failureCB(err);
       }
-  }
-  xhr.open('GET', url, true);
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.setRequestHeader('cache-control', 'no-cache');
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("cache-control", "no-cache");
   xhr.send(null);
-}
-
+};
